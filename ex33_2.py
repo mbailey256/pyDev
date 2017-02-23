2fce6a6ecb0ba3244caf53be1e92ca50e5f495b7
mbailey256 <mbaileysteel@yahoo.com>
2017-02-23 05:08:07 +0000

    updated ex33_2

--- ex33_2.py
+++ ex33_2.py
@@ -1,16 +1,43 @@
-i = 0
-numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+from sys import argv
 
-    i = i + 1
-    print "Numbers now: " , numbers
-    print "At the bottom i is %d" % i
+script,var1 = argv
+var2 = 10
+var1 = int(var1)
+print var1 + var1
+print var2 + var2
 
+#
+# i = 0
+# numbers = []
+#
+# #print var1
+#
+# def monkey(value):
+#     print 'Your value is', value
+#
+# # monkey(var1)
+#
+# while i < var1:
+#     print var1
+#
+#     i = i + 1
+#     print i
+#     if i == 10:
+#         break
 
-print "The numbers: "
-
-for num in numbers:
-    print num
+#
+# while i < var1:
+#     print "At the top i is %d" % i
+#     numbers.append(i)
+#
+#     i = i + 1
+#     print "Numbers now: " , numbers
+#     print "At the bottom i is %d" % i
+#
+#
+#
+# print "The numbers: "
+#
+# for num in numbers:
+#     print num
